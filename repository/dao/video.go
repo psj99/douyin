6a3ad3d0cfc35e4353b96b72b3615e75f2bd8fea
Chenghao-Tan <tch888free@gmail.com>
2023-08-16 01:23:42 +0800

    fix: Correct VideoFeed

--- repository/dao/video.go
+++ repository/dao/video.go
@@ -14,9 +14,9 @@ func FindVideosByUpdatedAt(ctx context.Context, updatedAt int64, forward bool, n
 	DB := GetDB(ctx)
 	stop := time.Unix(updatedAt, 0)
 	if forward {
-		err = DB.Model(&model.Video{}).Where("updated_at>=?", stop).Order("updated_at").Limit(num).Preload(clause.Associations).Find(&videos).Error
+		err = DB.Model(&model.Video{}).Where("updated_at>?", stop).Order("updated_at").Limit(num).Preload(clause.Associations).Find(&videos).Error
 	} else {
-		err = DB.Model(&model.Video{}).Where("updated_at<=?", stop).Order("updated_at desc").Limit(num).Preload(clause.Associations).Find(&videos).Error
+		err = DB.Model(&model.Video{}).Where("updated_at<?", stop).Order("updated_at desc").Limit(num).Preload(clause.Associations).Find(&videos).Error
 	}
 	return videos, err
 }
