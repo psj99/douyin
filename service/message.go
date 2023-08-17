6c3415c5d735a8f064da53c04c6c568310805d7d
Chenghao-Tan <tch888free@gmail.com>
2023-08-17 14:52:19 +0800

    chore: Improve message service comments

--- service/message.go
+++ service/message.go
@@ -15,27 +15,28 @@ import (
 
 func Message(ctx *gin.Context, req *request.MessageReq) (resp *response.MessageResp, err error) {
 	// 获取请求用户ID
-	Me_ID, ok := ctx.Get("user_id")
+	req_id, ok := ctx.Get("user_id")
 	if !ok {
-		utils.ZapLogger.Errorf("ctx.Get (user_id) err: inaccessible")
-		return nil, errors.New("无法获取user_id")
+		utils.ZapLogger.Errorf("ctx.Get (user_id) err: %v", err)
+		return nil, errors.New("无法获取请求用户ID")
 	}
 
 	// 获取目标用户ID
-	to_user_id, err := strconv.ParseUint(req.To_User_ID, 10, 64) // string转十进制uint64
+	to_user_id, err := strconv.ParseUint(req.To_User_ID, 10, 64)
 	if err != nil {
 		utils.ZapLogger.Errorf("ParseUint err: %v", err)
 		return nil, err
 	}
 
-	// 发送消息
+	// 操作消息
 	action_type, err := strconv.ParseUint(req.Action_Type, 10, 64)
 	if err != nil {
 		utils.ZapLogger.Errorf("ParseUint err: %v", err)
 		return nil, err
 	}
 	if action_type == 1 {
-		_, err := dao.CreateMessage(context.TODO(), Me_ID.(uint), uint(to_user_id), req.Content)
+		// 发送消息 //TODO
+		_, err := dao.CreateMessage(context.TODO(), req_id.(uint), uint(to_user_id), req.Content)
 		if err != nil {
 			utils.ZapLogger.Errorf("CreateMessage err: %v", err)
 			return nil, err
@@ -50,34 +51,35 @@ func Message(ctx *gin.Context, req *request.MessageReq) (resp *response.MessageR
 
 func MessageList(ctx *gin.Context, req *request.MessageListReq) (resp *response.MessageListResp, err error) {
 	// 获取请求用户ID
-	Me_ID, ok := ctx.Get("user_id")
+	req_id, ok := ctx.Get("user_id")
 	if !ok {
-		utils.ZapLogger.Errorf("ctx.Get (user_id) err: inaccessible")
-		return nil, errors.New("无法获取user_id")
+		utils.ZapLogger.Errorf("ctx.Get (user_id) err: %v", err)
+		return nil, errors.New("无法获取请求用户ID")
 	}
 
 	// 获取目标用户ID
-	to_user_id, err := strconv.ParseUint(req.To_User_ID, 10, 64) // string转十进制uint64
+	to_user_id, err := strconv.ParseUint(req.To_User_ID, 10, 64)
 	if err != nil {
 		utils.ZapLogger.Errorf("ParseUint err: %v", err)
 		return nil, err
 	}
 
 	// 读取消息列表
-	outMessages, err1 := dao.FindMessagesBy_From_To_ID(context.TODO(), Me_ID.(uint), uint(to_user_id), int64(req.Pre_Msg_Time), true, -1) // 查找从某刻起新消息 不限制数量
+	outMessages, err1 := dao.FindMessagesBy_From_To_ID(context.TODO(), req_id.(uint), uint(to_user_id), int64(req.Pre_Msg_Time), true, -1) // 查找从某刻起目标用户发送新消息 不限制数量
 	if err1 != nil {
 		utils.ZapLogger.Errorf("FindMessagesBy_From_To_ID err: %v", err)
 		return nil, err1
 	}
-	inMessages, err2 := dao.FindMessagesBy_From_To_ID(context.TODO(), uint(to_user_id), Me_ID.(uint), int64(req.Pre_Msg_Time), true, -1) // 查找从某刻起新消息 不限制数量
+	inMessages, err2 := dao.FindMessagesBy_From_To_ID(context.TODO(), uint(to_user_id), req_id.(uint), int64(req.Pre_Msg_Time), true, -1) // 查找从某刻起目标用户接收新消息 不限制数量
 	if err2 != nil {
 		utils.ZapLogger.Errorf("FindMessagesBy_From_To_ID err: %v", err)
 		return nil, err2
 	}
-	messages := append(outMessages, inMessages...)
+	messages := append(outMessages, inMessages...) // 合并二者
 
-	resp = &response.MessageListResp{}
+	resp = &response.MessageListResp{} // 初始化响应
 	for _, message := range messages {
+		// 初始化消息响应结构
 		messageInfo := response.Message{
 			ID:           message.ID,
 			To_User_ID:   message.ToUserID,
@@ -87,6 +89,7 @@ func MessageList(ctx *gin.Context, req *request.MessageListReq) (resp *response.
 			// Create_Time:  message.CreatedAt.Format("2006-01-02 15:04:05"),
 		}
 
+		// 将该消息加入列表
 		resp.Message_List = append(resp.Message_List, messageInfo)
 	}
 
